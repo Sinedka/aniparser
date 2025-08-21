6e27dcacfd9b2c7e63aaab93fe6e7f0014685dfc
Sinedka <dennnn8888@gmail.com>
2025-08-21 20:38:06 +0300

    some changes to scripts

--- prepare_aur.sh
+++ prepare_aur.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+ask_yes_no() {
+    prompt="$1"
+
+    printf "%s [y/n] " "$prompt"
+
+    # отключаем канонический ввод и эхо
+    stty -echo -icanon time 0 min 1
+    answer=$(dd bs=1 count=1 2>/dev/null)
+    stty sane
+
+    printf "%s\n" "$answer"
+
+    case "$answer" in
+        [Yy]) return 0 ;;  # Да
+        *)    return 1 ;;  # Нет
+    esac
+}
+
 # Цвета для вывода
 RED='\033[0;31m'
 GREEN='\033[0;32m'
@@ -8,13 +26,13 @@ NC='\033[0m'
 
 # Конфигурация
 PROJECT_NAME="aniparser"
-AUR_REPO="https://aur.archlinux.org/aniparser.git"
+AUR_REPO="ssh://aur@aur.archlinux.org/aniparser.git"
 VERSION=$(grep '"version":' package.json | cut -d'"' -f4)
 
 echo -e "${BLUE}Preparing AUR package for ${PROJECT_NAME} version ${VERSION}${NC}"
 
 # Переходим в директорию AUR
-cd aur
+cd aur || git clone $AUR_REPO aur && cd aur || echo "${RED}wtf is going on?${RED}"
 
 # Обновляем версию в PKGBUILD если необходимо
 sed -i "s/pkgver=.*/pkgver=${VERSION}/" PKGBUILD
@@ -31,3 +49,9 @@ echo -e "   cd aur"
 echo -e "   git add ."
 echo -e "   git commit -m \"Update to version ${VERSION}\""
 echo -e "   git push" 
+if ask_yes_no "exec them?"; then
+  cd aur || echo "${RED}wtf is going on?${RED}"
+  git add .
+  git commit -m "Update to version ${VERSION}"
+  git push
+fi
--- prepare_release.sh
+++ prepare_release.sh
@@ -1,4 +1,5 @@
 npm run dist:win
 npm run dist:linux
+cp -r dist-react dist-electron
 tar -czvf dist-electron.tar.gz dist-electron
 
