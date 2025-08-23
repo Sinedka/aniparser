610b41796e27303776e5be4baf177423ee7b3cd1
Sinedka <dennnn8888@gmail.com>
2025-08-23 22:06:50 +0300

    added setname

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -2,14 +2,13 @@ import { app, BrowserWindow } from "electron";
 import path from "path";
 import { isDev } from "./utils.js";
 
+app.setName("aniparser")
 app.whenReady().then(() => {
   const mainWindow = new BrowserWindow({
     transparent: process.platform === "win32" ? false : true,
     frame: process.platform === "win32" ? true : false,
     width: 800,
     height: 600,
-    title: "test",
-    icon: path.join(app.getAppPath(), "assets/icon.png"),
     webPreferences: {
       webSecurity: false,
     },
