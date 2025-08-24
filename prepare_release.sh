28940d0c9123f2f1b86a5ced831f801c61a1c35c
Sinedka <dennnn8888@gmail.com>
2025-08-24 15:30:03 +0300

    some fixes

--- prepare_release.sh
+++ prepare_release.sh
@@ -1,7 +1,5 @@
 npm run dist:win
 npm run dist:linux
 cp -r dist-react dist-electron
-cp -r dist-react assets/icon.svg
-cp -r dist-react assets/icon.png
+cp -r assets/icon.png dist-electron
 tar -czvf dist-electron.tar.gz dist-electron
-
