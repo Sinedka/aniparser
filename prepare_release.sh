e1d3d2a67bd283c0834494c5b3dae28affeaba75
Sinedka <123822966+Sinedka@users.noreply.github.com>
2025-08-28 20:38:06 +0300

    Update prepare_release.sh

--- prepare_release.sh
+++ prepare_release.sh
@@ -4,5 +4,5 @@ mkdir dist-full
 cp -r dist-react dist-full
 cp -r dist-electron dist-full
 cp -r assets/icon.png dist-full
-cp -r package_aur.json dist-full/package.json
+cp -r package-aur.json dist-full/package.json
 tar -czvf dist-full.tar.gz dist-full
