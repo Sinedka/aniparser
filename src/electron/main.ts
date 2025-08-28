1213c3b432bb0db92ffeee003fae65d8f03ae20f
Sinedka <dennnn8888@gmail.com>
2025-08-28 16:25:58 +0300

    added window state saving

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,15 +1,23 @@
 import { app, BrowserWindow, ipcMain } from "electron";
 import path from "path";
 import { isDev } from "./utils.js";
+import windowStateKeeper from "electron-window-state";
 
 let mainWindow: BrowserWindow | null = null;
 
 app.whenReady().then(() => {
+  let state = windowStateKeeper({
+    defaultWidth: 800,   // используется только если нет сохранённого состояния
+    defaultHeight: 600
+  });
+
   mainWindow = new BrowserWindow({
     transparent: process.platform === "win32" ? false : true,
     frame: false,
-    width: 800,
-    height: 600,
+    x: state.x,
+    y: state.y,
+    width: state.width,
+    height: state.height,
     webPreferences: {
       preload: __dirname + '/preload.js', // подключаем preload
       webSecurity: false,
@@ -22,6 +30,17 @@ app.whenReady().then(() => {
     mainWindow.loadURL("http://localhost:5123");
   } else {
     mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
+  }
+
+  // следим за состоянием окна
+  state.manage(mainWindow);
+
+  // если в прошлый раз окно было maximized → снова разворачиваем
+  if (state.isMaximized) {
+    mainWindow.maximize();
+  } else if (!state.x && !state.y) {
+    // если координат нет (т.е. первый запуск) → открываем сразу maximized
+    mainWindow.maximize();
   }
 
   mainWindow?.on("maximize", () => {
